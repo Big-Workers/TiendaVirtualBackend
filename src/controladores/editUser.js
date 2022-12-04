64cee5d57cd7a7d60ab9e678db936393ee0b9bf9
DESKTOP-6T6J8QA\Deivid <davidral33@hotmail.com>
2022-12-04 01:37:30 -0500

    Función editar usuario terminada

--- src/controladores/editUser.js
+++ src/controladores/editUser.js
@@ -5,61 +5,44 @@ const editUser = async (req, res) => {
     const { _id } = req.params
     const { nombre, email, telefono, direccion, contraseña } = req.body;
 
-    const userToEdit = await usuario.findOne({ email })
+    const userForEmail = await usuario.findOne({ email })
 
-    console.log({ userToEdit });
+    function modificarUser (user) {
+        bcrypt.hash(contraseña, 10, (error, contraseñaHasheada) => {
+            if (error) res.json({ error });
+            else {
+                usuario.updateOne({ _id: _id }, {
+                    nombre: nombre,
+                    email: email,
+                    telefono: telefono,
+                    direccion: direccion,
+                    contraseña: contraseñaHasheada
+                })
+                    .then((user) => {
+                        res.json({ mensaje: 'Usuario modificado correctamente', usuario });
+                    })
+                    .catch(error => console.error(error));
+            }
+        });
+    }
 
     usuario.findById({ _id }).then((user) => {
-
-        console.log("usuarioPorId" + user._id, "usuarioPorEmail" + userToEdit._id)
-
         if (!user) {
             return res.json({ mensaje: "El usuario no existe" });
+
         } else if (!nombre || !email || !telefono || !direccion || !contraseña) {
             return res.json({ mensaje: "Faltan datos del usuario" });
-        } else if (user._id === userToEdit._id) {
-            bcrypt.hash(contraseña, 10, (error, contraseñaHasheada) => {
-                if (error) res.json({ error });
-                else {
-                    const usuarioEditado = new usuario({
-                        nombre,
-                        email,
-                        telefono,
-                        direccion,
-                        contraseña: contraseñaHasheada
-                    });
-                    usuario.updateOne({ _id: _id }, { usuarioEditado })
-                        .then((user) => {
-                            res.json({ mensaje: 'Usuario modificado correctamente', user });
-                        })
-                        .catch(error => console.error(error));
-                }
 
-            });
-        } else {
-            return res.json({ mensaje: "El email ingresado corresponde a otro usuario" })
+        } else if (!userForEmail) {
+            modificarUser(user)
+
+        } else if (_id == userForEmail._id){
+            modificarUser(user)
+
+        } else {    
+            return res.json({ mensaje: "El email ingresado ya existe" })
         }
     });
-    /*
-    bcrypt.hash(contraseña, 10, (error, contraseñaHasheada) => {
-        if (error) res.json({ error });
-        else {
-            usuario.updateOne({ _id: _id }, {
-                nombre: nombre,
-                email: email,
-                telefono: telefono,
-                direccion: direccion,
-                contraseña: contraseñaHasheada
-            },
-                console.log({ usuario }),
-                function (err, usuario) {
-                    if (err) return console.err(err);
-                    else {
-                        res.json({ mensaje: 'Usuario modificado correctamente', usuario });
-                    }
-                })
-        }
-    })*/
 };
 
 module.exports = editUser;
