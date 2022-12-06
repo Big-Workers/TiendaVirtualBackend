7eaf11802626ece59a395122988fb1a17e290e65
DESKTOP-6T6J8QA\Deivid <davidral33@hotmail.com>
2022-12-05 22:27:00 -0500

    Correcciónes

--- src/controladores/editUser.js
+++ src/controladores/editUser.js
@@ -2,47 +2,47 @@ const bcrypt = require('bcrypt');
 const usuario = require("../modelo/usuario");
 
 const editUser = async (req, res) => {
-    const { _id } = req.params
-    const { nombre, email, telefono, direccion, contraseña } = req.body;
+  const { _id } = req.params
+  const { nombre, email, telefono, direccion, contraseña } = req.body;
 
-    const userForEmail = await usuario.findOne({ email })
+  const userForEmail = await usuario.findOne({ email })
 
-    function modificarUser (user) {
-        bcrypt.hash(contraseña, 10, (error, contraseñaHasheada) => {
-            if (error) res.json({ error });
-            else {
-                usuario.updateOne({ _id: _id }, {
-                    nombre: nombre,
-                    email: email,
-                    telefono: telefono,
-                    direccion: direccion,
-                    contraseña: contraseñaHasheada
-                })
-                    .then((user) => {
-                        res.json({ mensaje: 'Usuario modificado correctamente', usuario });
-                    })
-                    .catch(error => console.error(error));
-            }
-        });
-    }
-
-    usuario.findById({ _id }).then((user) => {
-        if (!user) {
-            return res.json({ mensaje: "El usuario no existe" });
-
-        } else if (!nombre || !email || !telefono || !direccion || !contraseña) {
-            return res.json({ mensaje: "Faltan datos del usuario" });
-
-        } else if (!userForEmail) {
-            modificarUser(user)
-
-        } else if (_id == userForEmail._id){
-            modificarUser(user)
-
-        } else {    
-            return res.json({ mensaje: "El email ingresado ya existe" })
-        }
+  function modificarUser(user) {
+    bcrypt.hash(contraseña, 10, (error, contraseñaHasheada) => {
+      if (error) res.json({ error });
+      else {
+        usuario.updateOne({ _id: _id }, {
+          nombre: nombre,
+          email: email,
+          telefono: telefono,
+          direccion: direccion,
+          contraseña: contraseñaHasheada
+        })
+          .then((user) => {
+            res.json({ mensaje: 'Usuario modificado correctamente', usuario });
+          })
+          .catch(error => console.error(error));
+      }
     });
+  }
+
+  usuario.findById({ _id }).then((user) => {
+    if (!user) {
+      return res.json({ mensaje: "El usuario no existe" });
+
+    } else if (!nombre || !email || !telefono || !direccion || !contraseña) {
+      return res.json({ mensaje: "Faltan datos del usuario" });
+
+    } else if (!userForEmail) {
+      modificarUser(user)
+
+    } else if (_id == userForEmail._id) {
+      modificarUser(user)
+
+    } else {
+      return res.json({ mensaje: "El email ingresado ya existe" })
+    }
+  });
 };
 
 module.exports = editUser;
--- src/controladores/getUserById.js
+++ src/controladores/getUserById.js
@@ -1,20 +1,19 @@
 const Usuario = require("../modelo/usuario");
 
 const getUserById = async (req, res) => {
-    const {_id}= req.params
+  const { _id } = req.params
 
-    if(_id.length === 24){
-        Usuario.findById(_id).then((user)=>{
-            if(!user){
-                return res.json({mensaje: "No se encontró ningún usuario con esa ID"});
-            } else {
-                const{ _id, contraseña, __v, ...resto } = user
-                res.json(user);
-            }
-        });
-    } else {
-        res.json ({ mensaje: " ID de usuario incorrecto"});        
-    }
+  if (_id.length === 24) {
+    Usuario.findById(_id).then((user) => {
+      if (!user) {
+        return res.json({ mensaje: "No se encontró ningún usuario con esa ID" });
+      } else {
+        res.json(user);
+      }
+    });
+  } else {
+    res.json({ mensaje: " ID de usuario incorrecto" });
+  }
 };
 
 module.exports = getUserById;
